04c2fe3a002c43101ff7c027bb6c8b0d34f2b674
whitejamer <mihailkuzin.job@yandex.ru>
2020-03-15 22:16:28 +0300

    Make Models

---
+++ app/models/Art.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\models;
+use Illuminate\Database\Eloquent\Model;
+
+class Art extends Model
+{
+    /**
+     * Art Model
+     * @int id
+     * @text image_url
+     * @text description
+     * @integer views
+     * @integer user_id
+     */
+
+    protected $guarded = ['views', 'user_id'];
+
+
+}
---
+++ app/models/StaticString.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StaticString extends Model
+{
+    /**
+     * StaticString Model
+     * Отвечает за статическую информацию на страницах
+     */
+}
